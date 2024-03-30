2c8a41075c3c12ad9f6060cbb77d106fa66c0328
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-03-30 15:57:08 +1300

    Fixed up nanoff command line for Spakfun ESP32 ThingPlus

--- BreakOutSerial/Program.cs
+++ BreakOutSerial/Program.cs
@@ -18,7 +18,7 @@
 #define SERIAL_ASYNC_READ
 //#define SERIAL_THREADED_READ
 //#define ST_STM32F769I_DISCOVERY      // nanoff --target ST_STM32F769I_DISCOVERY --update 
-//#define SPARKFUN_ESP32_THING_PLUS   // nanoff --platform esp32 --serialport COM4
+//#define SPARKFUN_ESP32_THING_PLUS   // nanoff --platform esp32 --serialport COM4 --update
 #define RAK_WISBLOCK_RAK2305 // nanoff --update --platform esp32 --serialport COM4 --update
 // May 2022 Still experiencing issues with ComPort assignments
 //#define NETDUINO3_WIFI   // nanoff --target NETDUINO3_WIFI --update
